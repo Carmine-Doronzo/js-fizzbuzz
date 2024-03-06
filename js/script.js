10bf8d86877c191a94d88e573dc5f856a62f57e3
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-03-06 16:21:05 +0100

    ADD bonus 1 e bonus 2

--- js/script.js
+++ js/script.js
@@ -14,13 +14,20 @@ BONUS 2:
 Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 */
 
+//dichiariamo la variabile container per renderla utilizzabile dal codice
+
+const elementContainer = document.querySelector('.container')
+
+
+
+
 //inizializziamo il ciclo for dichiaranto la variabile 'i' impostando la condizione 'i<100' e impostando il contatore in modo che aggiunga 1 unita' per ciclo
 for(let i = 0; i < 100; i++){
     //se vogliamo che ci stampi i valori da 1 a 100 dichiarare la variabile 'num' in modo che gia al primo ciclo risulti 1 e non zero
     let num = i + 1;
     
     //stampiamo in console la variabile num per 100 volte
-    
+    //console.log(num);
     
     //dichiariamo la variabile 'rest3' e 'rest5' per l'operazione modulo che ci dovra' dare come resto 0 se la variabile num e' divisibile per 3 o per 5
     let rest3 = num % 3;
@@ -28,18 +35,27 @@ for(let i = 0; i < 100; i++){
     
     //impostiamo la condizione: SE rest3 = 0 AND rest5 = 0 stampiamo fizzbuzz
     if(rest3 == 0 && rest5 == 0){
+        //stampa fizzbuzz
         console.log('FIZZBUZZ');
-    //impostiamo la condizione: SE rest3 = 0 stampiamo fizz
+        //creiamo un elemento nel dom piu' precisamente nel container contenente l'output 'fizzbuzz'
+        elementContainer.innerHTML += `<span class="fizzbuzz">FizzBuzz</span>`
+    //impostiamo la condizione:ALTRIMENTI SE rest3 = 0 stampiamo fizz
     }else if(rest3 == 0 ){
-        
+        //stampa fizz
         console.log('Fizz');
-    
-    //impostiamo la condizione: SE rest5 = 0 stampiamo buzz
+        //creiamo un elemento nel dom piu' precisamente nel container contenente l'output 'fizz'
+        elementContainer.innerHTML += `<span class="fizz">Fizz</span>`
+    //impostiamo la condizione:ALTRIMENTI SE rest5 = 0 stampiamo buzz
     }else if(rest5 == 0){
-
+        //stampa buzz
         console.log('Buzz');
+        //creiamo un elemento nel dom piu' precisamente nel container contenente l'output 'buzz'
+        elementContainer.innerHTML += `<span class="buzz">Buzz</span>`
     // ALTRIMENTI se le condizioni precedenti sono false stampiamo la variabile 'num'
     }else{
+        //stampa variabile num
         console.log(num)
+        //creiamo un elemento nel dom piu' precisamente nel container contenente come output la variabile 'num'
+        elementContainer.innerHTML += `<span class="num">${num}</span>`
     }
 }
